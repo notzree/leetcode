070b01a632440469d534028956f265d51088dd2d
Richard Zhang <118649285+notzree@users.noreply.github.com>
2023-06-26 19:55:37 -0400

    Time: 141 ms (48.30%) | Memory: 18.4 MB (52.99%) - LeetSync

--- trapping-rain-water/trapping-rain-water.py
+++ trapping-rain-water/trapping-rain-water.py
@@ -9,12 +9,12 @@ class Solution:
                 #only consider left side as left is the bounding
                 left = left +1
                 lMax = max(lMax,height[left])
-                print("Left"+ str(lMax - height[left]))
+                
                 water += lMax - height[left]
             else:
                 right = right -1
                 rMax = max(rMax,height[right])
-                print("Right"+ str(rMax-height[right]))
+                
                 water += rMax-height[right]
         return water
 

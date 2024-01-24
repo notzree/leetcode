92ab4e21d3fef3706fb0321fb41358e832a1577c
Richard Zhang <118649285+notzree@users.noreply.github.com>
2024-01-24 12:52:30 -0500

    Time: 45 ms (27.06%) | Memory: 16.6 MB (63.97%) - LeetSync

---
+++ 78-subsets/subsets.py
@@ -0,0 +1,31 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        # res = []
+        # subset = []
+        # def dfs(i):
+        #     if i >= len(nums):
+        #         res.append(subset[:])
+        #         return
+        #     #Decision to include nums[i]
+        #     subset.append(nums[i])
+        #     dfs(i+1)
+
+        #     #Decision to skip nums[i]
+        #     subset.pop()
+        #     dfs(i+1)
+        # dfs(0)
+        # return res
+
+        #Me trying the BFS way
+        res = [[]]
+        for n in nums:
+            for i in range(len(res)):
+                subset = res[i][:]
+                subset.append(n)
+                res.append(subset)
+        return res
+                
+
+
+
+        

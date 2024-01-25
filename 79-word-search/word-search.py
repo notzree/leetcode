76f63a94d1b1779758442d6a1a30f769a0731d46
Richard Zhang <118649285+notzree@users.noreply.github.com>
2024-01-24 22:58:39 -0500

    Time: 3626 ms (79.52%) | Memory: 16.6 MB (61.47%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -8,15 +8,18 @@ class Solution:
                 return True
             if i >len(word):
                 return False
-            if row >= width or col >= height or row < 0 or col < 0 or board[row][col]!=word[i] or (row,col) in visited:
+            if row >= width or col >= height or row < 0 or col < 0 or board[row][col]!=word[i] or board [row][col] == "#":
                 return False
-            visited.add((row,col))
+            # visited.add((row,col))
+            board[row][col] = "#"
             res = solve(row+1, col, i+1) or solve(row-1, col, i+1) or solve(row, col+1, i+1) or  solve(row, col-1, i+1)
-            visited.remove((row,col))
+            # visited.remove((row,col))
+            board[row][col] = word[i]
             return res
         for w in range(width):
             for h in range(height):
-                if solve(w,h,0): return True
+                if board[w][h] == word[0]:
+                    if solve(w,h,0): return True
 
             
             

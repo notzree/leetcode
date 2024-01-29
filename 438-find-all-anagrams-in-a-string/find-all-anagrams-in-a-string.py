d2097606921e88cae66775a4961df4859c8cf1da
Richard Zhang <118649285+notzree@users.noreply.github.com>
2024-01-28 20:04:00 -0500

    Time: 134 ms (50.56%) | Memory: 18 MB (60.31%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,39 +1,27 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        #iterate thru string p, append to dict like prev q
-        #if s[i] == any letter in p,
-        #check from i-> i+len(p) if characters match
-        #If it does record the index of i in list
-        # return list.
-        s_length, p_length = len(s), len(p)
-        if p_length > s_length:
+        if len(p) > len(s):
             return []
-        p_count, s_count = {}, {}
-        for i in range(p_length):
-            p_count[p[i]] = p_count.get(p[i],0)+1
-            s_count[s[i]] = s_count.get(s[i],0)+1
-        #sliding window initialized
-        res = [0] if s_count == p_count else []
-        left = 0
-        for right in range(p_length, s_length):
-            s_count[s[right]] = s_count.get(s[right],0)+1
-            s_count[s[left]] -=1
-
-            if s_count[s[left]]==0: #Pop this because if one hashmap has the key with value = 0 in, when comparing the hashmaps might not compare correctly
-                s_count.pop(s[left])
-            left+=1 #Update left pointer to reflect the current start index
-            if s_count == p_count:
-                res.append(left)
+        pfreq, substrfreq = {},{}
+        for c in range(len(p)):
+            pfreq[p[c]] = pfreq.get(p[c],0)+1
+            substrfreq[s[c]] = substrfreq.get(s[c],0)+1
+        res = [0] if pfreq == substrfreq else []
+        for c in range(len(p),len(s)):
+            print(c,c-len(p))
+            substrfreq[s[c]] = substrfreq.get(s[c],0)+1
+            substrfreq[s[c-len(p)]] -=1
+            if substrfreq[s[c-len(p)]] == 0:
+                substrfreq.pop(s[c-len(p)])
+            if substrfreq == pfreq:
+                res.append(c-len(p)+1)
         return res
-            
 
-            
+
+
+
 
         
-
-
-
-                    
-
-
-
+        
+        
+        

bc68e56fe66cb0db038ab17232896fa0d759546a
Richard Zhang <118649285+notzree@users.noreply.github.com>
2024-02-12 14:40:11 -0500

    Time: 10 ms (36.58%) | Memory: 47 MB (9.35%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,25 +1,27 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        ArrayList<int[]> output = new ArrayList<>();
+        Stack<int[]> output = new Stack<>();
         output.add(intervals[0]);
 
         for (int i = 1; i <intervals.length; i++){
             int [] pair = intervals[i];
-            int[] last_pair = output.get(output.size()-1);   
+            int[] last_pair = output.peek();  
             int last_endpoint = last_pair[1];
 
             if (pair[0]<=last_endpoint){
                 //merge them
-                output.get(output.size()-1)[1] = Math.max(last_endpoint,pair[1]);
+                output.pop();
+                last_pair[1] = Math.max(last_endpoint,pair[1]);
+                output.push(last_pair);
             }
             else {
-                output.add(pair);
+                output.push(pair);
             }
         }
         int[][] array = new int[output.size()][2];
-        for (int i = 0; i < output.size(); i++) {
-            int[] array1 = output.get(i);
+        for (int i = output.size()-1; i >=0; i--) {
+            int[] array1 = output.pop();
             array[i] = array1.clone(); // Creates a deep copy of each array
         }
         return array;

d4fa63128f42cd6d37cede390b7a8bcf08198034
Richard Zhang <118649285+notzree@users.noreply.github.com>
2024-07-12 16:46:12 -0700

    Time: 291 ms (28.77%) | Memory: 18.1 MB (75.15%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,20 +1,26 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        minEatingSpeed = 1
-        maxEatingSpeed = max(piles)
-        result = maxEatingSpeed
-
-        while (minEatingSpeed <= maxEatingSpeed):
-            currEatingSpeed = (minEatingSpeed+maxEatingSpeed)//2
+        def eat(speed: int) -> bool:
             hours = 0
-            for b in piles:
-                hours+= math.ceil(b/currEatingSpeed)
-            if hours > h:
-                minEatingSpeed = currEatingSpeed+1
-            elif hours<=h:
-                result = min(result, currEatingSpeed)
-                maxEatingSpeed = currEatingSpeed-1
-        return minEatingSpeed
+            for p in piles:
+                hours += -(-p//speed)
+            return hours <= h
+        min_eating_speed = 1
+        # her maximum eating speed is the max num of banans
+        max_eating_speed = max(piles)
+        result = max_eating_speed
+        while min_eating_speed <max_eating_speed:
+            current_speed = (min_eating_speed+max_eating_speed)//2
+            if eat(current_speed):
+                result = min(result, current_speed)
+                #current speed is less than or equal to given speed
+                #Can go slower
+                max_eating_speed = current_speed
+            else:
+                min_eating_speed = current_speed+1
+        return result
+
+
 
 
 

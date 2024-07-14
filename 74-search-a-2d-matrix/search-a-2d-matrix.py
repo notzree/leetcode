f3b1176d42cdcc4bf45f05e4ce7de645a978fe60
Richard Zhang <118649285+notzree@users.noreply.github.com>
2024-07-14 10:16:38 -0700

    Time: 48 ms (40.43%) | Memory: 17.1 MB (51.23%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -2,28 +2,22 @@ class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         #some sort of 2d binary search
         ROWS, COLS = len(matrix), len(matrix[0])
-
-        top, bot = 0, ROWS-1
-        while top <= bot:
-            row = (top+bot)//2
-            if target > matrix[row][-1]: # Need to search closer to bot
-                top = row+1
-            elif target < matrix[row][0]: # Need to search closer to top
-                bot = row -1
-            else:
-                break
-        if not (top<=bot): return False
-        row = (top+bot)//2
-        left, right = 0, COLS-1
-        while left <=right:
-            col = (left + right) //2
+        left, right = 0, (ROWS*COLS)-1
+        while left <= right:
+            total = (left + right)//2
+            row = total // COLS
+            col = total - (row*COLS)
             if matrix[row][col] == target:
                 return True
-            elif target > matrix[row][col]: # Need check smaller half
-                left = col+1
+            elif matrix[row][col] > target:
+                # need to search lower
+                right = total-1
             else:
-                right = col-1
+                left = total+1
         return False
+
+
+
                 
         
 

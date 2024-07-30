d3afe7c9408d1151008c24e4bc81a3b8c25ab1af
Richard Zhang <118649285+notzree@users.noreply.github.com>
2024-07-29 22:08:23 -0400

    Time: 38 ms (50.64%) | Memory: 16.8 MB (20.16%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,30 +1,17 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        subset = []
-        def dfs(i):
-            if i >= len(nums):
-                res.append(subset[:])
+        self.s = set()
+        def populate(combination, idx):
+            if idx == len(nums):
+                self.s.add(tuple(combination))
                 return
-            #Decision to include nums[i]
-            subset.append(nums[i])
-            dfs(i+1)
+            populate(combination, idx+1)
+            populate(combination + [nums[idx]],idx+1)
+        populate([], 0)
+        return self.s
 
-            #Decision to skip nums[i]
-            subset.pop()
-            dfs(i+1)
-        dfs(0)
-        return res
 
-        #Me trying the BFS way
-        # # u start with [] -> copy and add [n] --> [],[1],[2],[3] repeat.
-        # res = [[]]
-        # for n in nums:
-        #     for i in range(len(res)):
-        #         subset = res[i][:]
-        #         subset.append(n)
-        #         res.append(subset)
-        # return res
+
                 
 
 

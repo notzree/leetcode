96cfe497bcfe0e8ed0fa7fe58d45e89ec8e5f237
Richard Zhang <118649285+notzree@users.noreply.github.com>
2024-08-03 20:14:50 -0400

    Time: 6 ms (51.96%) | Memory: 5.3 MB (78.41%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.go
+++ 98-validate-binary-search-tree/validate-binary-search-tree.go
@@ -23,8 +23,14 @@ func dfs (root *TreeNode, low, high *int) bool{
     //left child
     //when we search the left, we expect that the left child is not greater than the current node
     // or in other words, leftChild <currentNode. We need to make sure the leftChild is less than the current node, but also 
-    //if the left node is on a right subtree, it is not smaller than it's parent.
+    //When we go to the left, we keep our left boundary the same, but update thright boundary.
+    // low < leftChild < currentNode
 
     //when we search the right, we expect that the right child is not less than the current node
+    //currentNode < rightChild < high
     return dfs(root.Left, low, &root.Val) && dfs(root.Right, &root.Val, high)
+
+    //example on [5,1,4,null,null,3,6]
+    //When we go from 5->4, our range is: x > 5 (update low)
+    //when we go from 4->3, our range is:  5<x<4 (update high) which is impossible
 }

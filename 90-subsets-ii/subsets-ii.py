d048ee8678a2a76f10c08163ce8bbbb99bd17bc8
Richard Zhang <118649285+notzree@users.noreply.github.com>
2024-08-04 18:58:55 -0400

    Time: 41 ms (45.89%) | Memory: 16.8 MB (37.29%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -1,21 +1,27 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        res = []
-        subset= []
-        def dfs(i):
-            if i >= len(nums):
-                res.append(subset[:])
+        result = []
+        subset = []
+        def dfs(i: int):
+            if i == len(nums):
+                result.append(subset[:])
                 return
+            # All subsets that contain nums[i]
             subset.append(nums[i])
             dfs(i+1)
+            # All subsets that do not contain nums[i]
+            # We need to sort it so that duplicates will be beside each other
             subset.pop()
-
-            while i+1 < len(nums) and (nums[i]==nums[i+1]) :
+            while i+1 < len(nums) and nums[i] == nums[i+1]:
                 i+=1
             dfs(i+1)
         dfs(0)
-        return res
+        return result
+            
+
+
+
 
         
         

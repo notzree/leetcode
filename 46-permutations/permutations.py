4313595ebd57353c94924b6b624bdc5f89620708
Richard Zhang <118649285+notzree@users.noreply.github.com>
2024-08-04 18:13:28 -0400

    Time: 43 ms (40.33%) | Memory: 16.6 MB (88.16%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,15 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        perms = [[]]
+        if len(nums) ==1:
+            return [nums[:]]
+        for n in nums:
+            for i in range(len(perms)):
+                p = perms.pop(0) # Remove the existing permutation
+                for j in range(len(p)+1):
+                    p_copy = p.copy()
+                    p_copy.insert(j,n)
+                    perms.append(p_copy)
+        return perms
+
+

951100c49b7820a3781fd2e8b3e618e3a30fa3de
Richard Zhang <118649285+notzree@users.noreply.github.com>
2024-08-06 22:34:10 -0400

    Time: 7264 ms (7.00%) | Memory: 16.8 MB (22.53%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,25 +1,27 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        width = len(board)
-        height = len(board[0])
-        visited = set()
-        def solve(row, col, i):
-            if i==len(word):
+        def dfs(row: int, col: int, index: int):
+            if row not in range(len(board)) or col not in range(len(board[0])) or board[row][col] !=word[index]:
+                return False
+            if index == len(word)-1:
                 return True
-            if i >len(word):
-                return False
-            if row >= width or col >= height or row < 0 or col < 0 or board[row][col]!=word[i] or board [row][col] == "#":
-                return False
-            # visited.add((row,col))
+            temp = board[row][col]
             board[row][col] = "#"
-            res = solve(row+1, col, i+1) or solve(row-1, col, i+1) or solve(row, col+1, i+1) or  solve(row, col-1, i+1)
-            # visited.remove((row,col))
-            board[row][col] = word[i]
-            return res
-        for w in range(width):
-            for h in range(height):
-                if board[w][h] == word[0]:
-                    if solve(w,h,0): return True
+            for x_incr, y_incr in directions:
+                new_row = row+y_incr
+                new_col = col+x_incr
+                if dfs(new_row, new_col, index+1):
+                    return True
+            board[row][col] = temp
+            return False
+
+        directions = [(0,1), (1,0), (0, -1), (-1,0)]
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                if board[r][c] == word[0]:
+                    if dfs(r,c,0):
+                        return True
+        return False
 
             
             

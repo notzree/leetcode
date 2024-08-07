c41f7da948a580ad2d20dfea132af7f28a0727ac
Richard Zhang <118649285+notzree@users.noreply.github.com>
2024-08-06 23:13:59 -0400

    Time: 53 ms (68.61%) | Memory: 16.6 MB (59.79%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -5,9 +5,7 @@ class Solution:
         prev_one = cost[1] # starting from 1
         prev_two = cost[0] # starting from 0
         for i in range(2,len(cost)):
-            current_cost = min(prev_one, prev_two) + cost[i]
-            prev_two = prev_one
-            prev_one = current_cost
+            prev_one, prev_two = min(prev_one, prev_two) + cost[i], prev_one
         return min(prev_one, prev_two)
 
 
